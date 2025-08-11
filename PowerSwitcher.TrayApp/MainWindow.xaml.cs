9172a6e3343ff2a3b0a4e2797d1caf1c2e18c545
chaixshot <chai5142xshot@gmail.com>
2025-08-11 15:15:25 +0700

    change: apply WPF-UI

--- PowerSwitcher.TrayApp/MainWindow.xaml.cs
+++ PowerSwitcher.TrayApp/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Wpf.Ui.Controls;
 
 namespace PowerSwitcher.TrayApp
 {
@@ -15,7 +16,7 @@ namespace PowerSwitcher.TrayApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
